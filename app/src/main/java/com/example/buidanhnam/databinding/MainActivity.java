46870b960095175ed541f878325f73948572376e
NamBD <bui.danh.nam@framgia.com>
2017-12-22 14:30:58 +0700

    fix undifine

--- app/src/main/java/com/example/buidanhnam/databinding/MainActivity.java
+++ app/src/main/java/com/example/buidanhnam/databinding/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.buidanhnam.databinding;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+  LinearLayout linearLayout = new LinearLayout();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linearLayout.measure();
     }
 }
